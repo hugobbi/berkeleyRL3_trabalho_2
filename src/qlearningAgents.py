80124d70488d5cd98644b41d555bac9ca12a7f4a
hugobbi <hgugobbi@gmail.com>
2023-07-26 14:27:16 -0300

    removendo print

--- src/qlearningAgents.py
+++ src/qlearningAgents.py
@@ -233,5 +233,5 @@ class ApproximateQAgent(PacmanQAgent):
         if self.episodesSoFar == self.numTraining:
             # you might want to print your weights here for debugging
             "*** YOUR CODE HERE ***"
-            print(f"{self.getWeights()=}")
+            #print(f"{self.getWeights()=}")
             pass
